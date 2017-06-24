35436b4ff8bb22618bc8f56c63648e03223d526f
Ben Abrams <me@benabrams.it>
2017-06-24 11:40:07 -0700

    closes #3 by bumping depedencies
    There are several motivations for this the most important being able to use newer than 1.x ES gem and limiting it to the 2 major gems (some thing might be broken and anyways I would consider this a breaking change).
    
    gem deps bumped:
    - `elasticsearch (['>= 2.0','<= 6.0'])`
    - `faraday (~> 0.12)`
    - `faraday_middleware (~> 0.11.0.1)`
    
    As part of this just to get everything to work in a modern version of ruby I had to do some other restructuring here are some of the misc changes:
    - restructured gem lib structure (in order remove lots of require_relatives that were broken in ruby > 2)
    - made versioning a bit easier to work with
    
    Signed-off-by: Ben Abrams <me@benabrams.it>

---
+++ lib/aws-es-transport.rb
@@ -0,0 +1,4 @@
+require 'aws-es-transport/version'
+require 'aws-es-transport/aws_v4_signer_impl'
+require 'aws-es-transport/aws_v4_signer'
+# require 'aws-es-transport/elasticsearch/transport/transport/http/aws'
--- lib/aws-es-transport/aws_v4_signer.rb
+++ lib/aws-es-transport/aws_v4_signer.rb
@@ -1,5 +1,5 @@
 require 'faraday'
-require_relative 'aws_v4_signer_impl'
+require 'aws-es-transport/aws_v4_signer_impl'
 
 module FaradayMiddleware
   Faraday::Request.register_middleware aws_v4_signer: lambda { AwsV4Signer }
